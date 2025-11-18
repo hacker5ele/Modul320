5a4540ef78870e1870af76f03f4ca2a177ae894d
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-11-18 09:05:12 +0100

    create inheritance file

---
+++ V1/src/first/Animal.java
@@ -0,0 +1,18 @@
+package first;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String sound;
+
+    public Animal(String name, int age, String sound) {
+        this.name = name;
+        this.age = age;
+        this.sound = sound;
+    }
+
+    public void makeSound() {
+        System.out.println(sound);
+    }
+
+}
