1c245ebc21370ec37a9fe055262d14a1521d4492
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-11-25 08:34:24 +0100

    add V1 code and structure

---
+++ V2/src/first/Animal.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Sound");
+    }
+
+}
